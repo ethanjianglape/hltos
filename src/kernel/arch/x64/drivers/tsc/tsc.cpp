8296e7162ffb74b4c052ffe350c69e9768e9a159
Ethan Jiang-Lape <ethanjianglape@gmail.com>
2026-07-24 21:47:44 -0400

    cleanup tsc.cpp a bit

--- src/kernel/arch/x64/drivers/tsc/tsc.cpp
+++ src/kernel/arch/x64/drivers/tsc/tsc.cpp
@@ -9,8 +9,6 @@
 
 namespace x64::drivers::tsc {
 
-constexpr std::uint32_t CPUID_FEAT_EDX_INVARIANT_TSC = (1 << 8); // Invariant TSC available
-
 static std::uint64_t boot_tsc = 0;
 static std::uint64_t tsc_freq = 0;
 
@@ -58,6 +56,8 @@ std::uint64_t get_tsc_freq()
 // Invariant TSC support is determined by CPUID.0x80000007.EDX[8]
 static bool check_invariant_tsc_support()
 {
+    constexpr std::uint32_t CPUID_FEAT_EDX_INVARIANT_TSC = (1 << 8);
+
     std::uint32_t eax;
     std::uint32_t edx;
 
@@ -74,17 +74,17 @@ void init()
 
     boot_tsc = rdtsc();
 
-    const auto t0 = boot_tsc;
-    const auto sleep_ms = 20;
-    const auto total_ms = 100;
+    const std::uint64_t t0 = boot_tsc;
+    const std::uint32_t sleep_ms = 20;
+    const std::uint32_t total_ms = 100;
 
     // sleep for a total of 100ms (5 x 20ms)
     // note: the PIT physically cannot sleep for longer than ~54ms at a time
-    for (int i = 0; i < total_ms / sleep_ms; i++) {
+    for (std::uint32_t i = 0; i < total_ms / sleep_ms; i++) {
         x64::drivers::pit::sleep_ms(sleep_ms);
     }
 
-    const auto t1 = rdtsc();
+    const std::uint64_t t1 = rdtsc();
 
     tsc_freq = ((t1 - t0) * 1000) / total_ms;
 
